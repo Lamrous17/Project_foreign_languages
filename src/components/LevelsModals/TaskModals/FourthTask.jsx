293709dff1cd92ba1dbd24362c127a9deb93240f
lamrous17 <122687360+Lamrous17@users.noreply.github.com>
2024-07-06 15:14:09 +0700

    Update fourth task with add changes
    + checking value from input

--- src/components/LevelsModals/TaskModals/FourthTask.jsx
+++ src/components/LevelsModals/TaskModals/FourthTask.jsx
@@ -1,11 +1,28 @@
+import React, { useState } from 'react';
+import toast from 'react-hot-toast';
 import { CloseTaskBtn, StyledTaskClose, LeftBtn, LeftBtnImg, TranslationLevelModal, TranslationTaskName, TranslationTaskForm, TranslationTaskFormInput, TranslationTaskFormSubmit, TranslationTaskDescription, TranslationTaskGoal } from "../LevelsModals.js";
-import React, { useState, useEffect } from 'react';
-import leftArrow from "../../../assets/images/LeftArrow.png"
-import tasksData from "./tasks.json"; 
+import leftArrow from "../../../assets/images/LeftArrow.png";
+import tasksData from "./tasks.json";
 
 const FourthTask = ({ closeModal, handleLeftClick }) => {
-  const vocabularyItems = tasksData.task[0].russian
+  const [userInput, setUserInput] = useState('');
+  const correctAnswer = tasksData.task[0].english;
+  const [showToast, setShowToast] = useState(false);
 
+  const handleInputChange = (event) => {
+    setUserInput(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (userInput.trim().toLowerCase() === correctAnswer.toLowerCase()) {
+      setShowToast(true);
+      toast.success('Верно!');
+    } else {
+      setShowToast(false);
+      toast.error('Неверно. Попробуйте еще!');
+    }
+  };
 
   return (
     <TranslationLevelModal>
@@ -15,18 +32,21 @@ const FourthTask = ({ closeModal, handleLeftClick }) => {
       <TranslationTaskDescription>
         Напишите текст на английском языке:
       </TranslationTaskDescription>
-      <TranslationTaskGoal>{vocabularyItems} -</TranslationTaskGoal>
+      <TranslationTaskGoal>{tasksData.task[0].russian} -</TranslationTaskGoal>
 
-      <TranslationTaskForm name="videoAnswer">
-        <TranslationTaskFormInput type="text" />
+      <TranslationTaskForm name="videoAnswer" onSubmit={handleFormSubmit}>
+        <TranslationTaskFormInput
+          type="text"
+          value={userInput}
+          onChange={handleInputChange}
+          required
+        />
         <TranslationTaskFormSubmit type="submit">Отправить</TranslationTaskFormSubmit>
       </TranslationTaskForm>
-      
+
       <LeftBtn onClick={handleLeftClick} type="button"><LeftBtnImg src={leftArrow} alt="" /></LeftBtn>
     </TranslationLevelModal>
-  )
-}
+  );
+};
 
-export default FourthTask
-
-
+export default FourthTask;
